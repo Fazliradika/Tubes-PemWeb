2a308d2efdc64b0207600aa487aa95470ec0d9b8
Fazliradika <fazliradika30april@gmail.com>
2025-11-29 14:45:39 +0700

    Add register/login links on auth pages for better navigation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -58,13 +58,19 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
             <x-primary-button class="ms-4">
                 {{ __('Register') }}
             </x-primary-button>
+        </div>
+
+        <!-- Login Link -->
+        <div class="mt-6 text-center border-t pt-6">
+            <p class="text-sm text-gray-600">
+                {{ __('Already have an account?') }}
+                <a class="font-semibold text-blue-600 hover:text-blue-800 underline" href="{{ route('login') }}">
+                    {{ __('Login here') }}
+                </a>
+            </p>
         </div>
     </form>
 </x-guest-layout>
