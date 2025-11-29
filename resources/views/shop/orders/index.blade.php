2550d1f753b756a8b10ba10c75a2e155217b65f6
Fazliradika <fazliradika30april@gmail.com>
2025-11-29 14:55:17 +0700

    Fix Blade syntax error in orders index page

--- resources/views/shop/orders/index.blade.php
+++ resources/views/shop/orders/index.blade.php
@@ -52,12 +52,15 @@
                                         <div class="space-y-2">
                                             @foreach($order->orderItems->take(2) as $item)
                                                 <div class="flex items-center space-x-3">
-                                                    <img src="{{ $item->product->image ?? 'https://images.unsplash.com/photo-1584308666744-24d5c474f2ae?w=150&h=150&fit=crop' }}" 
-                                                        alt="{{ $item->product_name }}" 
-                                                        class="w-12 h-12 object-cover rounded">
+                                                    @if($item->product && $item->product->image)
+                                                        <img src="{{ $item->product->image }}" 
+                                                            alt="{{ $item->product_name }}" 
+                                                            class="w-12 h-12 object-cover rounded">
                                                     @else
                                                         <div class="w-12 h-12 bg-gray-200 rounded flex items-center justify-center">
-                                                            <i class="fas fa-image text-gray-400"></i>
+                                                            <svg class="w-6 h-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                                            </svg>
                                                         </div>
                                                     @endif
                                                     <div class="flex-1">
